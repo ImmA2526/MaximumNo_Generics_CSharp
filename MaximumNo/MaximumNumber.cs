8a2d07e20a9a9bd6e12a6f500bce77075a41a1b8
Avenger2627 <imraninfo97@gmail.com>
2020-12-15 12:06:42 +0530

    [Imran Shaikh] Add: Maximum Method

--- MaximumNo/MaximumNumber.cs
+++ MaximumNo/MaximumNumber.cs
@@ -6,14 +6,13 @@ namespace MaximumNo
 {
     public class MaximumNumber<T> where T:IComparable
     {
-        public T firstNumber,secondNumber,thirdNumber;
-        
-        public MaximumNumber(T firstNumber, T secondNumber, T thirdNumber)
+        public T[] Number;
+        public MaximumNumber(T [] Number)
         {
-            this.firstNumber = firstNumber;
-            this.secondNumber = secondNumber;
-            this.thirdNumber = thirdNumber;
+            this.Number = Number;
+            
         }
+
         public static T MaxNumber(T firstNumber, T secondNumber, T thirdNumber) 
         {
             if (firstNumber.CompareTo(secondNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
@@ -32,38 +31,29 @@ namespace MaximumNo
             throw new Exception("firstNumber,secondNumber,thirdNumber Same");
         }
 
-        public static T MaxFloatNumber(T firstNumber, T secondNumber, T thirdNumber)
+        public T[] Sort(T[] Number)
         {
-            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
-            {
-                return firstNumber;
-            }
-            if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
-            {
-                return secondNumber;
-            }
-            if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
-            {
-                return thirdNumber;
-            }
-            throw new Exception("firstNumber,secondNumber,thirdNumber Same.");
+            Array.Sort(Number);
+            return Number;
         }
 
-        public static T StringMaximumNumber(T S1, T S2, T S3) 
+        public T MaximumValue(params T[] Number)
         {
-            if (S1.CompareTo(S2) > 0 && S1.CompareTo(S3) > 0)
-            {
-                return S1;
-            }
-            if (S2.CompareTo(S1) > 0 && S2.CompareTo(S3) > 0)
-            {
-                return S2;
-            }
-            if (S3.CompareTo(S1) > 0 && S3.CompareTo(S2) > 0)
-            {
-                return S3;
-            }
-            throw new Exception("S1,S2,S3 are Same ");
+            var SorteNumber = Sort(Number);
+            return SorteNumber[^1];
+        }
+
+        public T MaxMethod()
+        {
+            var MaxValue=MaximumValue(this.Number);
+            return MaxValue;
+        }
+
+        public void PrintValue()
+        {
+            var max = MaximumValue(this.Number);
+            Console.Write(max);
+
         }
     }
 }
